f801c9f5d8ace93255e7747617a8d57a91f8a8ac
Turki <alola.turki@gmail.com>
2019-09-16 16:02:15 +0300

    more panel imporvments

--- src/components/UserPanel/panelListing.js
+++ src/components/UserPanel/panelListing.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
 import Vision from '../Assets/logo.ico'
+import "./panelListing.css"
 
 export default class Listing extends React.Component{
     
@@ -30,17 +31,19 @@ export default class Listing extends React.Component{
     render(){
         return(
             <div>
-             <ListGroup flush >
-        <ListGroupItem href="#" tag="button"   style={{backgroundColor: "transparent", margin: "10px 15px 10px 10px"}} >
+             <ListGroup flush>
+        <ListGroupItem  tag="button" id = "listing"   >
             <img src={Vision} alt="" style={{width: "64px", 
                                             height: "64px",
                                             position: "relative",
                                             float: "left"
                                             }}/>
+            <div style={{textAlign: "left", marginLeft: "14%", position: "relative"}}>
           <ListGroupItemHeading >{this.props.name} </ListGroupItemHeading>
-          <ListGroupItemText>
+          <ListGroupItemText style={{marginLeft: "px"}}>
           {this.props.date}
           </ListGroupItemText>
+            </div>
         </ListGroupItem>
       </ListGroup>
       </div>
